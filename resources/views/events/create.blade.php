a1ca143b3a47b7e55cace707569ff52b76399448
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-06-02 22:44:33 -0300

    Salvando dados no banco MySQL - #15

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,37 @@
 
 @section('content')
 
-    <h1>Crie um evento</h1>
+    <div id="event-create-container" class="col-md-4 offset-md-4">
+        <h1>Crie o seu evento</h1>
+
+        <form action="/events" method="post">
+            @csrf   <!-- essa diretiva é para poder enviar os dados do formulaŕio -->
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            </div>
+
+            <div class="form-group">
+                <label for="city">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+            </div>
+
+            <div class="form-group">
+                <label for="private">O evento é privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="description">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+            </div>
+
+            <input id="btn-criar-evento" type="submit" class="btn btn-primary" value="Criar Evento">
+
+        </form>
+    </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
-
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact/{id?}', function ($id = null) {
     return view('contact', ['id' => $id]);
